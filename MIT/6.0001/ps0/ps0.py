f255f9367984d651347791a16b72587cd18b7865
CoderNoah <113279627+codernoahx@users.noreply.github.com>
2026-01-19 20:23:16 +0530

    minor changes in ps0

--- MIT/6.0001/ps0/ps0.py
+++ MIT/6.0001/ps0/ps0.py
@@ -1,7 +1,15 @@
 import math
 
-x = int(input("Enter number x: "))
-y = int(input("Enter number y: "))
 
-print(f"X**y = {x ** y}")
-print(f"log(x) = {math.log2(x)}")
+def main():
+    x = int(input("Enter number x: "))
+    y = int(input("Enter number y: "))
+
+    # x raised to the power y
+    print(f"X**y = {x ** y}")
+    # log base 2 of x
+    print(f"log(x) = {int(math.log2(x))}")
+
+
+if __name__ == "__main__":
+    main()
