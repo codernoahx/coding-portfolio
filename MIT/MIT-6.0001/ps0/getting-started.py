c6e2932b2ca30ece221d0cd72d8c4af778861bcf
CoderNoah <113279627+codernoahx@users.noreply.github.com>
2026-01-19 19:52:31 +0530

    getting started exercises

---
+++ MIT/MIT-6.0001/ps0/getting-started.py
@@ -0,0 +1,20 @@
+import math
+import numpy
+import matplotlib
+
+
+def main():
+    print(16**0.5)
+    print(math.sqrt(16))
+    print(math.log(16, 2))
+    print(math.cos(0))
+    print(6 + 4 * 10)
+    print((6 + 4) * 10)
+    print(23.0**5)
+    a, b, c = 34, 68, -510
+    print((-b + (b * b - 4 * a * c) ** 0.5) / (2 * a))
+    print(math.cos(3.4) ** 2 + math.sin(3.4) ** 2)
+
+
+if __name__ == "__main__":
+    main()
